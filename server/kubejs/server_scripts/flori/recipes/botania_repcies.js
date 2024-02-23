82094ad30615a2bd860247096f6c32f728891c9e
肉鸡二号 <rookies_two@yeah.net>
2024-02-23 18:11:37 +0800

    移除残留的植物魔法配方

--- server/kubejs/server_scripts/flori/recipes/botania_repcies.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-    event.recipes.botania.petal_apothecary('kubejs:nightmare_carrot', ['kubejs:dread_ascarid', 'kubejs:dread_ascarid', 'kubejs:drome_nugget', 'kubejs:drome_nugget', 'kubejs:drome_nugget', 'kubejs:drome_nugget', 'minecraft:phantom_membrane', 'minecraft:phantom_membrane', 'minecraft:golden_carrot']);
-})
